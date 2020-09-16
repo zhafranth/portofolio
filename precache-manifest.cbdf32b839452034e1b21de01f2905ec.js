48d44279e7acc807998392c774a0400fab064f92
Zhafranth <zhafranth@gmail.com>
2020-09-16 09:34:22 +0700

    Updates

---
+++ precache-manifest.cbdf32b839452034e1b21de01f2905ec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a299ef93aa1bab43f6e2f06b0e18fa9b",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "40c947b0bf759e1d15c0",
+    "url": "/portofolio/static/css/2.d0affde7.chunk.css"
+  },
+  {
+    "revision": "a8a6cbe5cd965eb6eb42",
+    "url": "/portofolio/static/css/main.7393a74a.chunk.css"
+  },
+  {
+    "revision": "40c947b0bf759e1d15c0",
+    "url": "/portofolio/static/js/2.11c939a7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portofolio/static/js/2.11c939a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a6cbe5cd965eb6eb42",
+    "url": "/portofolio/static/js/main.8166069e.chunk.js"
+  },
+  {
+    "revision": "a3a6c8a1f81f721d2b09",
+    "url": "/portofolio/static/js/runtime-main.cef59536.js"
+  },
+  {
+    "revision": "e1b87887563f92e4518b750b1a904f9f",
+    "url": "/portofolio/static/media/lingkaran1.e1b87887.png"
+  },
+  {
+    "revision": "6e2b8a0c504492baa00e9fdaae77e515",
+    "url": "/portofolio/static/media/lingkaran2.6e2b8a0c.png"
+  },
+  {
+    "revision": "d3fc01ff11a0f568ffafd800f2fbcdbb",
+    "url": "/portofolio/static/media/lingkaran3.d3fc01ff.png"
+  },
+  {
+    "revision": "271abda6e02eaa1e0c0ea26c4163e81b",
+    "url": "/portofolio/static/media/logo.271abda6.svg"
+  }
+]);
